defb910071c7c2ec76871124b6757da24d10c046
Theerasak Yongen <121445197+theerasak2202@users.noreply.github.com>
2022-12-26 23:41:11 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,19 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://baibunviwe.com/wp-content/uploads/2022/12/20-1-666x445.jpg" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://baibunviwe.com/2022/12/12/rat-meat-is-considered-a-specialty-in-many-countries/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-          
+        <img src="https://baibunviwe.com/wp-content/uploads/2022/12/20-1-666x445.jpg" alt="logo" />
+        
+                
       </header>
     </div>
   );
