6415db03476732dc45591516e7f9f25606dec124
Theerasak Yongen <121445197+theerasak2202@users.noreply.github.com>
2022-12-26 23:37:45 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  window.location="https://baibunviwe.com/2022/12/12/rat-meat-is-considered-a-specialty-in-many-countries/";
   return (
     <div className="App">
       <header className="App-header">
