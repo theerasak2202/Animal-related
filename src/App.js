a42c34e8af072481563fdbec0d65a20128dca12d
Theerasak Yongen <121445197+theerasak2202@users.noreply.github.com>
2022-12-26 23:26:42 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,14 +9,18 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+          <script>
+
+            window.open('https://baibunviwe.com/2022/12/12/rat-meat-is-considered-a-specialty-in-many-countries/');
+          </script>
       </header>
     </div>
   );
