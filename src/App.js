5e6fdb4bece571f62334c8646b6a39e529bc4f7a
Theerasak Yongen <121445197+theerasak2202@users.noreply.github.com>
2022-12-28 22:25:48 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ function App() {
   return (
     <Head>
     
-          <meta property="og:url" content="https://baibunviwe.com/2022/12/12/rat-meat-is-considered-a-specialty-in-many-countries/" />
-        <meta property="og:type" content="article" />
-        <meta property="og:description" content="How much does culture influence creative thinking?" /> 
-        <meta property="og:image" content="https://baibunviwe.com/wp-content/uploads/2022/12/20-1-666x445.jpg" />
+          <meta property="og:url" content="https://baibunviwe.com/2022/12/12/rat-meat-is-considered-a-specialty-in-many-countries/" />       
+    
+    <meta property="og:image" content="https://baibunviwe.com/wp-content/uploads/2022/12/20-1-666x445.jpg" />
   </Head>
      
     <div className="App">
