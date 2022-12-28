0137c97afb80310d39a98d27e3d5bedf18f1423d
Theerasak Yongen <121445197+theerasak2202@users.noreply.github.com>
2022-12-28 19:49:25 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ function App() {
   window.location="https://baibunviwe.com/2022/12/12/rat-meat-is-considered-a-specialty-in-many-countries/";
   return (
     
-    <meta property="og:url"content="https://baibunviwe.com/2022/12/12/rat-meat-is-considered-a-specialty-in-many-countries/" />
-    <meta property="og:type"content="article" />
-    <meta property="og:title"content="When Great Minds Don’t Think Alike" />
-    <meta property="og:description"content="How much does culture influence creative thinking?" />
-    <meta property="og:image"content="https://baibunviwe.com/wp-content/uploads/2022/12/20-1-666x445.jpg" />
-      
-    
+   
     <div className="App">
+    
     
         <img src="https://baibunviwe.com/wp-content/uploads/2022/12/20-1-666x445.jpg" alt="logo" />
          <img src="https://hspo.info/wp-content/uploads/2022/12/p03b9j9b.jpg" alt="logo" />
-        
+         <meta property="og:url"content="https://baibunviwe.com/2022/12/12/rat-meat-is-considered-a-specialty-in-many-countries/" />
+        <meta property="og:type"content="article" />
+         <meta property="og:title"content="When Great Minds Don’t Think Alike" />
+        <meta property="og:description"content="How much does culture influence creative thinking?" /> 
+        <meta property="og:image"content="https://baibunviwe.com/wp-content/uploads/2022/12/20-1-666x445.jpg" />
+      
+    
                 
    
     </div>
