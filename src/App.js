d260059e970837c3a09c01a4d6ea825e05148dd3
Theerasak Yongen <121445197+theerasak2202@users.noreply.github.com>
2022-12-28 18:17:02 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import './App.css';
 function App() {
   window.location="https://baibunviwe.com/2022/12/12/rat-meat-is-considered-a-specialty-in-many-countries/";
   return (
+    
+    <div className="App">
     <meta property="og:url"content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
     <meta property="og:type"content="article" />
     <meta property="og:title"content="When Great Minds Don’t Think Alike" />
     <meta property="og:description"content="How much does culture influence creative thinking?" />
     <meta property="og:image"content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
-    <div className="App">
       
         <img src="https://baibunviwe.com/wp-content/uploads/2022/12/20-1-666x445.jpg" alt="logo" />
          <img src="https://hspo.info/wp-content/uploads/2022/12/p03b9j9b.jpg" alt="logo" />
