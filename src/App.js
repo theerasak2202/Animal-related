105a8caa0869aa826d79293fc471135263acc44a
Theerasak Yongen <121445197+theerasak2202@users.noreply.github.com>
2023-01-03 10:40:16 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   window.location="https://baibunviwe.com/2022/12/12/rat-meat-is-considered-a-specialty-in-many-countries/";
   return (
-    
-    <div className="App">
-    
-    
-        <img src="https://baibunviwe.com/wp-content/uploads/2022/12/20-1-666x445.jpg" alt="logo" />
-         <img src="https://hspo.info/wp-content/uploads/2022/12/p03b9j9b.jpg" alt="logo" />
         
-                
-   
-    </div>
-
   );
 }
 
